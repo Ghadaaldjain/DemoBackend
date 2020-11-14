dde1da6c92a468714be1b45bc0773845e9980571
Ghada Aldjain <ghadaaldjain@gmail.com>
2020-11-15 01:21:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 
 mobileApp.tables.add('Book');// Create a table for 'Book' with default settings
 mobileApp.tables.add('Users');
+mobileApp.tables.add('Product');
+mobileApp.tables.add('Category');
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
